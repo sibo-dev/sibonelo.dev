6d675d20ceef5ed53f7f33893760850c26e51284
DMA Software <info@dmasoftware.co.za>
2026-03-31 20:59:37 +0200

    feat: add new projects and images to portfolio
    - Updated index.html to reference new JavaScript file.
    - Added images for "ToDo" and "SiboCloudStorage" projects.
    - Introduced two new projects: "Laravel Task Manager" and "Laravel Cloud Storage" with descriptions, links, and images.
    - Adjusted existing project IDs to maintain uniqueness.

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -4,10 +4,36 @@ import matchaImg from "../assets/Matcha.png";
 // @ts-ignore
 import thorImg from "../assets/Thor.png";
 // import matchaLink from "../proj/build/index.html";
+// @ts-ignore
+import toDoImg from "../assets/ToDo.png";
+// @ts-ignore
+import cloudStorageImg from "../assets/SiboCloudStorage2.png";
 
 export const PROJECTS: Project[] = [
   {
     id: "1",
+    title: "Laravel Task Manager",
+    description:
+      "A very powerful task management application built with Laravel. It uses github to track issues you may be facing. UAC allows users to independantly manage their tasks. It allows users to create, update, and delete tasks, as well as mark them as completed.",
+    category: "Web",
+    tags: ["Laravel", "PHP", "MySQL"],
+    image: toDoImg,
+    link: "https://todo.sibonelo.dev/",
+    github: "https://github.com/sibo-dev/lara_task",
+  },
+  {
+    id: "2",
+    title: "Laravel Cloud Storage",
+    description:
+      "A cloud storage application built with Laravel. It allows users to upload, download, and manage their files in the cloud. It also has a feature that allows users to share their files with others. You can link it to an AWS Bucket for additional storage. This is one of the most complex project I have built and I update it from time to time with new features and improvements. \nUsername: demo@demo.com Password: Demo123!",
+    category: "Web",
+    tags: ["Laravel", "PHP", "MySQL"],
+    image: cloudStorageImg,
+    link: "https://cloud.sibonelo.dev/",
+    github: "https://github.com/sibo-dev/laravel-cloud-storage",
+  },
+  {
+    id: "5",
     title: "Matcha.com",
     description:
       "A modern dating app with matchmaking, chat, and location-based features. This product is no longer actively maintained, but you can check out the code on GitHub.",
@@ -18,7 +44,7 @@ export const PROJECTS: Project[] = [
     github: "https://github.com/sibo-dev/matcha",
   },
   {
-    id: "2",
+    id: "7",
     title: "Gap Brewer",
     description:
       "Gap Draught is a company that has its roots in the IoT space and was established in 2015. This app is for Brewery sales reps to retrieve live sales and pour data for all their registered customers",
@@ -29,7 +55,7 @@ export const PROJECTS: Project[] = [
     link: "https://play.google.com/store/apps/details?id=com.gapdraught.repapp",
   },
   {
-    id: "3",
+    id: "8",
     title: "Gap Bar",
     description:
       "Gap Bar is a mobile app for bar owners and managers to monitor their sales and pour data in real-time. It allows them to track inventory, manage staff and view detailed reports and analytics to optimize their operations and increase profitability.",
@@ -40,7 +66,7 @@ export const PROJECTS: Project[] = [
       "https://play-lh.googleusercontent.com/MovB_hAFZ1CSk49r_-4Z1xI3D1GGHCikYotnbeO2cFDdSjsXIkG1jKjhHxi1Mj77NrI=w240-h480-rw",
   },
   {
-    id: "4",
+    id: "9",
     title: "Gap Portal UK (Thor)",
     description:
       "A greenfields project that required building a custom made portal to replace a legacy PHP system. This React project uses many modern practices such as using the Context API over Redux. State management and other modern React practices were implemented to ensure a smooth user experience and efficient data handling.",
